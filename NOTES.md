5ec88dd179b0bbb73275d7d38e6b623583b32045
Robert Forkel <xrotwang@googlemail.com>
2024-04-24 13:17:57 +0200

    getting there

---
+++ NOTES.md
@@ -0,0 +1,53 @@
+
+This dataset is derived from [Files for Australian Language Locations](https://doi.org/10.5281/zenodo.4898185).
+Thus, all caveats mentioned in the description of this dataset apply here as well. Suitable use
+cases are described as
+
+> These maps should be considered as a way to relate linguistic groups to one another in space. It 
+> should help to visualize the approximate distances between groups, abstracting away from 
+> multilingualism, population density, and Indigenous settlement patterns.
+
+This dataset is derived as follows:
+
+- **Matching features to Glottolog languoids**: The features (aka polygons) in the source GeoJSON
+  are matched to the "nearest" Glottolog languoid in terms of the Glottolog classification. Thus,
+  if not identical match can be found, varieties are matched to their parent language in Glottolog,
+  and languages which are not attested in Glottolog, but the genealogy of which is known are matched
+  to the closest sub-group in Glottolog. The matching was done based on languoid name, genealogy and
+  location and is recorded at [etc/languages.csv](etc/languages.csv).
+- The main contribution of this dataset are two GeoJSON files:
+  - [cldf/languages.geojson](cldf/languages.geojson): The polygons found in the source are
+    aggregated on Glottolog language level. Thus, the features in this file represent speaker areas
+    for the respective Glottolog language-level languoids.
+  - [cldf/families.geojson](cldf/families.geojson): The polygons found in the source are
+    aggregated on Glottolog top-level family level. Considering that the language families covered
+    by this dataset are only represented in Australia and all Australian languges are supposed to
+    be covered here, the features in this file represent speaker areas for the respective Glottolog
+    family-level languoid.
+  - A third GeoJSON file [etc/uncoded.geojson](etc/uncoded.geojson) is supplied to allow inspection
+    of the features from the source for which no Glottolog match could be determined.
+
+
+### Usage
+
+Since the GIS data is linked to the CLDF dataset via the [speakerArea](https://github.com/cldf/cldf/tree/master/components/languages#speaker-area)
+property, you can access it programmatically, e.g. using `pycldf`:
+
+```python
+>>> from pycldf import Dataset
+>>> ds = Dataset.from_metadata('cldf/Generic-metadata.json')
+>>> pny = [l for l in ds.objects('LanguageTable') if l.cldf.glottocode == 'pama1250'][0]
+>>> import shapely
+>>> area = shapely.geometry.shape(pny.speaker_area_as_geojson_feature['geometry'])
+>>> area.contains(shapely.Point(140, -20))
+True
+>>> import json
+>>> geojson = json.dumps(pny.speaker_area_as_geojson_feature, indent=2)
+>>> print(geojson)
+```
+
+E.g. to display the area of the Pama-Nyungan family:
+
+```geojson
+{}
+```
--- cldfbench_bowernpnygeo.py
+++ cldfbench_bowernpnygeo.py
@@ -1,22 +1,17 @@
+import json
+import types
 import pathlib
 import subprocess
 import collections
 
-from shapely.geometry import shape, Point
+from shapely import union_all
+from shapely.geometry import shape
+from pycldf import Sources
 from cldfbench import Dataset as BaseDataset
-from cldfbench.datadir import DataDir
+from clldutils.color import qualitative_colors
+from clldutils.jsonlib import dump
+from clldutils.markup import add_markdown_text
 
-LCOLS = {
-    'glottolog@id': 'Glottocode',
-    'ISO639': 'ISO639P3code',
-    'StandardLanguageName': 'Name',
-    'Family': 'Family',
-    'Langs_Subgroups::SubgroupName': 'SubGroup',
-    'EtymologyofName': 'EtymologyOfName',
-    'Latitude': 'Latitude',
-    'Longitude': 'Longitude',
-    'AIATSIS_Code': 'AIATSIScode',
-}
 BASE_URL = "https://zenodo.org/record/4898185/files/"
 FILES = [
     'Chirila Language Codes.xlsx',
@@ -24,104 +19,41 @@ FILES = [
     'AustralianCentroids.kml',
     'AustralianLanguageFamilies.kml',
 ]
-# Matches by looking up point coordinates in polygons:
-KML_NAME_TO_STANDARD_NAME = {
-    "Karlaaku": "Galaagu",
-    "Ngajumaya": "Ngatjumaya",
-    "Wanamara": "Wunumara",
-    #"Kuuku Yani": "Umpithamu",
-    #"Kugu Nganhcara": "Kugu-Uwanh",
-    "Yirandhali": "Yirandali",
-    #"Minang": "Bibbulman",
-    #"Nyunga": "Wudjari",
-    "Mbeiwum": "Mbiywom",
-    #"Ndra'ngith": "Anguthimri",
-    #"Amangu": "Nganakarti",
-    "Mpalityan": "Mpalitjanh",
-    #"WakaWaka": "Gureng Gureng",
-    "Gubbi Gubbi": "Gabi-Gabi",
-    "Guyani": "Kuyani",
-    "Nunukal": "Nukunul",
-    #"Uw-Olgol": "Koko-Bera",
-    "Kuuk-Narr": "Kok-Nar",
-    #"Koko Bera": "Kuuku-Ya'u",
-    "Yir Yoront": "Yir-Yoront",
-    "Kuku Yalanji": "Kuku-Yalanji",
-    "YortaYorta": "Yorta Yorta",
-    "Gandangara": "Gundungurra",
-    "Nimanburu": "Nimanburru",
-    #"Kwini": "Gunin",
-    "Djadjawurung": "Djadjawurrung",
-    "MathiMathi": "Mathi-Mathi",
-    "Wemba Wemba": "Yabula-Yabula",
-    "Ngaliwuru": "Ngaliwurru",
-    "Magati Ke": "Mati Ke",
-    #"Marri Ammu": "Marrithiyel",
-    "Ladji Ladji": "Ladji-Ladji",
-    #"Yiman": "Waka-Waka",
-    #"Yangga": "Biri",
-    "Kokatha": "Kukatha",
-    #"Daungwurrung": "Wemba-Wemba",
-    "Djirbal": "Dyirbal",
-    #"Wirri": "Barada",
-    #"Wadjigu": "Gayiri",
-    "Anmatyerr": "Anmatyerre",
-    "Lower Aranda": "Lower Southern Aranda",
-    "Western Arrarnta": "Western Arrarnte",
-    "Eastern Arrernte": "Eastern and Central Arrernte",
-    #"Bigambal": "Githabul",
-    #"Wangkangurru": "Karangura",
-    "Darrkinyung": "Darkinyung",
-    "Thanggatti": "Thanggati",
-    "Eora": "Iyora",
-    "Gija": "Kija",
-    "Gajirrebeng": "Kajirrabeng",
-    #"Mengerrdji": "Erre",
-    "Urningangk": "Urningangga",
-    "Yaygirr": "Yaygir",
-    "Bininj Kunwok": "Bininj Gun-wok",
-    "Gudanji": "Gurdanji",
-    #"Tharrgari": "Thiin",
-    "Dungaloo": "Dhungaloo",
-    #"Yiningayi": "Wadjabangayi",
-    "Larrakia": "Larrakiya",
-    #"Ngarkat": "Ngayawang",
-    #"Ngaiawang": "Keramin",
-    "Raminyeri": "Ramindjeri",
-    #"Tharrayi": "Yingkarta",
-    "Nhanda": "Nhanta",
-    "Hunter River Lake Macquarie": "Hunter River and Lake Macquarie",
-    "Kungkarri": "Kungkari",
-    "Gaagudju": "Gagudju",
-    #"Gugu Badhun": "Gudjal",
-    #"Jiwarliny": "Yulparija",
-    #"Garandi": "Kuthant",
-    #"Southern Anaiwan": "Nganyaywana",
-    #"Ribh": "Kurtjar",
-    #"Gugu Djangun": "Mbabaram",
-    #"Wik-Alkan": "Wik-Ngatharr",
-    #"Wik-Alkan": "Wik-Me?nh",
-    #"Wik-Paacha": "Wik-Ngathan",
-    #"Kuuk Yak": "Kuuk Thaayorre",
-    "Takalak": "Tagalaka",
-    #"Athima": "Walangama",
-    #"Ogunyjan": "Olkola",
-    #"Menthe": "Emmi",
-    #"Yunggurr": "Matngele",
-    #"Tyerraty": "Kungarakany",
-    #"Marri Tjevin": "Marringarr",
-    "Nyaanyatjarra": "Ngaanyatjarra",
-    #"Omeo language": "Bidhawal",
-    #"Condamine- Upper-Clarence": "Bandjalang",
-    #"Lower Richmond": "Minjungbal",
-    "Guyambal": "Yugambal",
-    #"Awu Alaya": "Gugu-Rarmul",
-    #"Ogh-Alungul": "Gugu-Warra",
-    #"Ogh-Alungul": "Aghu-Tharnggala",
-    #"Ogh Awarrangg": "Kunjen",
-    #"Agu Aloja": "Kuuk-Yak",
-    "Southern Kaantju": "Kaanju",
-}
+
+
+def merged_geometry(features):
+    # Note: We slightly increase each polygon using `buffer` to make sure they overlap a bit and
+    # internal boundaries are thus removed.
+    return union_all([shape(f['geometry']).buffer(0.001) for f in features])
+
+
+class FeatureCollection(list):
+    def __init__(self, path, **properties):
+        self.path = path
+        self.properties = properties
+        list.__init__(self)
+
+    def __enter__(self):
+        return self
+
+    def append_feature(self, shape, **properties):
+        self.append(dict(type="Feature", properties=properties, geometry=shape.__geo_interface__))
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        geojson = dict(type="FeatureCollection", features=self, properties=self.properties)
+        dump(geojson, self.path, indent=2)
+
+    def as_row(self, **kw):
+        res = dict(
+            ID=self.path.stem,
+            Name=self.properties['title'],
+            Description=self.properties['description'],
+            Media_Type='application/geo+json',
+            Download_URL=self.path.name,
+        )
+        res.update(kw)
+        return res
+
 
 class Dataset(BaseDataset):
     dir = pathlib.Path(__file__).parent
@@ -129,6 +61,15 @@ class Dataset(BaseDataset):
 
     def cldf_specs(self):  # A dataset must declare all CLDF sets it creates.
         return super().cldf_specs()
+
+    def cmd_readme(self, args) -> str:
+        ds = self.cldf_reader()
+        pny = [l for l in ds.objects('LanguageTable') if l.cldf.glottocode == 'pama1250'][0]
+        geojson = json.dumps(pny.speaker_area_as_geojson_feature)
+        return add_markdown_text(
+            BaseDataset.cmd_readme(self, args),
+            self.dir.joinpath('NOTES.md').read_text(encoding='utf8').format(geojson),
+            'Description')
 
     def cmd_download(self, args):
         for f in FILES:
@@ -141,31 +82,151 @@ class Dataset(BaseDataset):
                     raise ValueError(p)
 
     def cmd_makecldf(self, args):
-        langs = {r['StandardLanguageName']: r
-                 for r in self.raw_dir.read_csv('Chirila_Language_Codes.Sheet1.csv', dicts=True)}
-        print(len(langs))
-        mlayer, mcat = collections.Counter(), collections.Counter()
-        missing = {}
-        for i, f in enumerate(self.raw_dir.read_json('AustralianPolygons.geojson')['features']):
-            props = f['properties']
-            props['name'] = KML_NAME_TO_STANDARD_NAME.get(props['name'], props['name'])
-            if props['name'] not in langs:
-                mlayer.update([props.get('layer')])
-                mcat.update([props.get('Dialect')])
-                missing[props['name']] = shape(f['geometry'])
-            else:
-                del langs[f['properties']['name']]
-        print(mlayer)
-        print(mcat)
-        print('{} unmatched Languages in Language_Codes sheet'.format(len(langs)))
-        for l in sorted(langs):
-            print(l)
+        self.schema(args.writer.cldf)
+        args.writer.cldf.add_sources(*Sources.from_file(self.etc_dir / 'sources.bib'))
 
-        print('{} unmatched Languages in AustralianPolygons'.format(len(missing)))
-        for name in sorted(missing):
-            print(name)
-        #    for lname, l in langs.items():
-        #        if l['RoundedLong'] and l['RoundedLat']:
-        #            p = Point(float(l['RoundedLong']), float(l['RoundedLat']))
-        #            if p.within(area):
-        #                print('    "{}": "{}",'.format(name, lname))
+        glangs = {lg.id: lg for lg in args.glottolog.api.languoids()}
+        lang2fam = {}
+        lang = {gc for gc, glang in glangs.items() if glang.level.name == 'language'}
+
+        polys_by_code = collections.defaultdict(list)
+        coded_langs = {
+            r['Name']: r for r in self.etc_dir.read_csv('languages.csv', dicts=True)
+            if r.get('Glottocode')}
+
+        # Assemble all Glottocodes related to any area.
+        uncoded = []
+        for i, f in enumerate(self.raw_dir.read_json('AustralianPolygons.geojson')['features']):
+            props = types.SimpleNamespace(**f['properties'])
+            args.writer.objects['ContributionTable'].append(dict(
+                ID=props.fid,
+                Name=props.name,
+                Layer=props.layer,
+                Pama_Nyungan=getattr(props, 'Family', '').lower() != 'nonpny',
+                Dialect=getattr(props, 'Dialect', '') == 'y',
+                Comment=getattr(props, 'description', None),
+                Source=['bowern_2021']
+            ))
+            if props.name in coded_langs:
+                glang = glangs[coded_langs[props.name]['Glottocode']]
+                polys_by_code[glang.id].append((props.fid, f))
+                lang2fam[glang.id] = glang.id if not glang.lineage else glang.lineage[0][1]
+                if glang.lineage:
+                    lang2fam[glang.id] = glang.lineage[0][1]
+                    for _, fgc, _ in glang.lineage:
+                        lang2fam[fgc] = glang.lineage[0][1]
+                        polys_by_code[fgc].append((props.fid, f))
+                else:
+                    lang2fam[glang.id] = glang.id
+            else:
+                uncoded.append(f)
+        dump(dict(type="FeatureCollection", features=uncoded),
+             self.etc_dir / 'uncoded.geojson', indent=2)
+
+        colors = dict(zip(
+            [k for k, v in collections.Counter(lang2fam.values()).most_common()],
+            qualitative_colors(len(lang2fam.values()))))
+
+        with FeatureCollection(
+            self.cldf_dir / 'languages.geojson',
+            title='Speaker areas for languages',
+            description='Speaker areas aggregated for Glottolog language-level languoids, '
+                        'color-coded by family.',
+        ) as geojson:
+            for gc, polys in polys_by_code.items():
+                glang = glangs[gc]
+                if gc in lang:
+                    args.writer.objects['LanguageTable'].append(dict(
+                        ID=gc,
+                        Name=glang.name,
+                        Glottocode=gc,
+                        Latitude=glang.latitude,
+                        Longitude=glang.longitude,
+                        Source_Languoid_IDs=[p[0] for p in polys],
+                        Speaker_Area=geojson.path.stem,
+                        Glottolog_Languoid_Level='language',
+                        Family=glangs[lang2fam[gc]].name if lang2fam[gc] != gc else None,
+                    ))
+                    geojson.append_feature(
+                        merged_geometry([p[1] for p in polys]),
+                        title=glang.name,
+                        fill=colors[lang2fam[gc]],
+                        family=glangs[lang2fam[gc]].name if lang2fam[gc] != gc else None,
+                        **{'cldf:languageReference': gc, 'fill-opacity': 0.8})
+        args.writer.objects['MediaTable'].append(geojson.as_row())
+
+        with FeatureCollection(
+            self.cldf_dir / 'families.geojson',
+            title='Speaker areas for language families',
+            description='Speaker areas aggregated for Glottolog top-level family languoids.',
+        ) as geojson:
+            for gc in sorted(set(lang2fam.values())):
+                glang = glangs[gc]
+                if gc not in lang:  # Don't append isolates twice!
+                    args.writer.objects['LanguageTable'].append(dict(
+                        ID=gc,
+                        Name=glang.name,
+                        Glottocode=gc,
+                        Source_Languoid_IDs=[p[0] for p in polys_by_code[gc]],
+                        Speaker_Area=geojson.path.stem,
+                        Glottolog_Languoid_Level='family',
+                    ))
+                geojson.append_feature(
+                    merged_geometry([p[1] for p in polys_by_code[gc]]),
+                    title=glang.name,
+                    fill=colors[gc],
+                    **{'cldf:languageReference': gc, "fill-opacity": 0.8})
+        args.writer.objects['MediaTable'].append(geojson.as_row())
+
+    def schema(self, cldf):
+        t = cldf.add_component(
+            'ContributionTable',
+            "Layer",
+            {
+                "name": "Pama_Nyungan",
+                "dc:description":
+                    "Flag signaling whether the variety is member of the Pama-Nyungan family.",
+                "datatype": {"base": "boolean", "format": "yes|no"},
+            },
+            {
+                "name": "Dialect",
+                "dc:description":
+                    "Flag signaling whether the variety is considered a dialect.",
+                "datatype": {"base": "boolean", "format": "yes|no"},
+            },
+            {
+                'name':'Comment',
+                'propertyUrl': 'http://cldf.clld.org/v1.0/terms.rdf#comment'
+            },
+            {
+                'name': 'Source',
+                'separator': ';',
+                'propertyUrl': 'http://cldf.clld.org/v1.0/terms.rdf#source'
+            },
+        )
+        t.common_props['dc:description'] = \
+            ('We list the individual shapes from the source dataset as contributions in order to '
+             'preserve the original metadata.')
+        cldf['ContributionTable', 'id'].common_props['dc:description'] = \
+            ('We use the 1-based index of the first shape with corresponding '
+             'LANGUAGE property in the original shapefile as identifier.')
+        cldf.add_component('MediaTable')
+        cldf.add_component(
+            'LanguageTable',
+            {
+                'name': 'Speaker_Area',
+                'propertyUrl': 'http://cldf.clld.org/v1.0/terms.rdf#speakerArea'
+            },
+            {
+                'name': 'Glottolog_Languoid_Level'},
+            {
+                'name': 'Family',
+            },
+            {
+                'name': 'Source_Languoid_IDs',
+                'separator': ' ',
+                'dc:description': 'List of identifiers of shapes in the original shapefile that '
+                                  'were aggregated to create the shape referenced by Speaker_Area.',
+                'propertyUrl': 'http://cldf.clld.org/v1.0/terms.rdf#contributionReference'
+            },
+        )
